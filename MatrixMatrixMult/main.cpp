5c2e272cdc02779fd116e44cbbb32d6734c1e9bd
Damlrca <sir.damirca@yandex.ru>
2023-11-07 00:41:52 +0300

    matrix compare

--- MatrixMatrixMult/main.cpp
+++ MatrixMatrixMult/main.cpp
@@ -13,6 +13,8 @@ extern "C" {
 #include <iomanip>
 using namespace std;
 
+constexpr double EPS = 1e-8;
+
 const char* filename_mtx = "D:\\source\\coursework\\Freescale1.mtx";
 const char* filename_bin = "D:\\source\\coursework\\Freescale1.bin";
 
@@ -84,6 +86,33 @@ void print_CSR_as_dense(const matrix_CSR& mtx, const char* mtx_name) {
 		}
 	}
 	print_dense(dense);
+}
+
+// difference of two matricies in ORDERED CSR format
+void matrix_difference(matrix_CSR* mtx1, matrix_CSR* mtx2, matrix_CSR* res) {
+	int nz = mtx2->row_id[mtx2->N];
+	for (int i = 0; i < nz; i++)
+		mtx2->value[i] = -mtx2->value[i];
+	matrix_addition(mtx1, mtx2, res);
+	for (int i = 0; i < nz; i++)
+		mtx2->value[i] = -mtx2->value[i];
+}
+
+// compare two matricies in ORDERED CSR format, using matrix_difference
+// true if matricies are equal
+bool matrix_compare(matrix_CSR* mtx1, matrix_CSR* mtx2) {
+	if (mtx1->N != mtx2->N || mtx1->M != mtx2->M)
+		return false;
+	matrix_CSR res;
+	matrix_difference(mtx1, mtx2, &res);
+	double mxdiff = 0;
+	int nz = res.row_id[res.N];
+	for (int i = 0; i < nz; i++) {
+		if (abs(res.value[i]) > abs(mxdiff))
+			mxdiff = res.value[i];
+	}
+	delete_CSR(&res);
+	return abs(mxdiff) < EPS;
 }
 
 void few_tests() {
@@ -176,63 +205,111 @@ void test_addition() {
 }
 
 void few_more_tests() {
-	/*
 	// read mtx matrix
-	cout << "read " << filename_mtx << endl;
-	MyTimer::SetStartTime();
 	matrix_COO mtx1;
+	cout << "(mtx1) read " << filename_mtx << endl;
+	MyTimer::SetStartTime();
 	if (read_matrix_MTX(filename_mtx, &mtx1) != 0) {
-		cout << "failed to read" << " " << filename_mtx << endl;
+		cout << "failed to read " << filename_mtx << endl;
 		exit(-1);
 	};
 	MyTimer::SetEndTime();
 	cout << "N :" << mtx1.N << ", M: " << mtx1.M << ", nz: " << mtx1.nz << endl;
-	cout << "matrix read succsessfully in ";
+	cout << "matrix read successfully in ";
 	MyTimer::PrintDifference();
 	cout << endl;
 	cout << "----------------------" << endl;
-	delete_COO(&mtx1);
-	*/
+	
 	// read bin matrix
-	cout << "read " << filename_bin << endl;
-	MyTimer::SetStartTime();
 	matrix_COO mtx2;
+	cout << "(mtx2) read " << filename_bin << endl;
+	MyTimer::SetStartTime();
 	if (read_matrix_BIN(filename_bin, &mtx2) != 0) {
 		cout << "failed to read" << " " << filename_bin << endl;
 		exit(-1);
 	};
 	MyTimer::SetEndTime();
 	cout << "N :" << mtx2.N << ", M: " << mtx2.M << ", nz: " << mtx2.nz << endl;
-	cout << "matrix read succsessfully in ";
+	cout << "matrix read successfully in ";
 	MyTimer::PrintDifference();
 	cout << endl;
 	cout << "----------------------" << endl;
 
-	// COO to CSR
-	cout << "convert COO to CSR" << endl;
+	// mtx COO to CSR
+	cout << "(mtx1->mtx3) convert COO to CSR" << endl;
 	matrix_CSR mtx3;
 	MyTimer::SetStartTime();
-	COO_to_CSR(&mtx2, &mtx3);
+	COO_to_CSR(&mtx1, &mtx3);
 	MyTimer::SetEndTime();
 	cout << "N :" << mtx3.N << ", M: " << mtx3.M << ", nz: " << mtx3.row_id[mtx3.N] << endl;
-	cout << "matrix converted succsessfully in ";
+	cout << "matrix converted successfully in ";
 	MyTimer::PrintDifference();
 	cout << endl;
 	cout << "----------------------" << endl;
 
-	// double transpose
+	// bin COO to CSR
+	cout << "(mtx2->mtx4) convert COO to CSR" << endl;
+	matrix_CSR mtx4;
 	MyTimer::SetStartTime();
-	transpose_this(&mtx3);
-	transpose_this(&mtx3);
+	COO_to_CSR(&mtx2, &mtx4);
 	MyTimer::SetEndTime();
-	cout << "N :" << mtx3.N << ", M: " << mtx3.M << ", nz: " << mtx3.row_id[mtx3.N] << endl;
-	cout << "matrix doudle transposed succsessfully in ";
+	cout << "N :" << mtx4.N << ", M: " << mtx4.M << ", nz: " << mtx4.row_id[mtx4.N] << endl;
+	cout << "matrix converted successfully in ";
 	MyTimer::PrintDifference();
 	cout << endl;
 	cout << "----------------------" << endl;
 
+	// compare matrcies mtx3 and mtx4
+	cout << "comparing mtx3 and mtx4" << endl;
+	bool result;
+	MyTimer::SetStartTime();
+	result = matrix_compare(&mtx3, &mtx4);
+	MyTimer::SetEndTime();
+	cout << "matricies compared in ";
+	MyTimer::PrintDifference();
+	cout << endl;
+	if (result) {
+		cout << "OK, mtx3 == mtx4" << endl;
+	}
+	else {
+		cout << "something failed: mtx3 != mtx4  :(" << endl;
+		exit(-1);
+	}
+	cout << "----------------------" << endl;
+
+	// double transpose mtx4
+	cout << "(mtx4) double transpose" << endl;
+	MyTimer::SetStartTime();
+	transpose_this(&mtx4);
+	transpose_this(&mtx4);
+	MyTimer::SetEndTime();
+	cout << "N :" << mtx4.N << ", M: " << mtx4.M << ", nz: " << mtx4.row_id[mtx4.N] << endl;
+	cout << "matrix double transposed successfully in ";
+	MyTimer::PrintDifference();
+	cout << endl;
+	cout << "----------------------" << endl;
+
+	// compare matricies mtx3 ans mtx4 after double transpose
+	cout << "comparing mtx3 and mtx4" << endl;
+	MyTimer::SetStartTime();
+	result = matrix_compare(&mtx3, &mtx4);
+	MyTimer::SetEndTime();
+	cout << "matricies compared in ";
+	MyTimer::PrintDifference();
+	cout << endl;
+	if (result) {
+		cout << "OK, mtx3 == mtx4" << endl;
+	}
+	else {
+		cout << "something failed: mtx3 != mtx4  :(" << endl;
+		exit(-1);
+	}
+	cout << "----------------------" << endl;
+
+	delete_COO(&mtx1);
 	delete_COO(&mtx2);
 	delete_CSR(&mtx3);
+	delete_CSR(&mtx4);
 }
 
 int main() {
@@ -246,54 +323,5 @@ int main() {
 	cout << "Testing some functions with larger matrices" << endl;
 	cout << "----------------------" << endl;
 	few_more_tests();
-	return 0;
-	/*
-	// check matrix-vector multiplication
-
-	// Generate vector V
-	double* V = new double[M];
-	std::random_device rd;
-	std::mt19937_64 gen(rd());
-	std::uniform_real_distribution<double> rand(-100.0, 100.0);
-	for (int i = 0; i < M; i++)
-		V[i] = rand(gen);
-
-	// one thread, direct
-	start_time = myclock::now();
-	double* Result_direct = new double[N];
-	memset(Result_direct, 0, sizeof(double) * N);
-	for (int i = 0; i < nz; i++) {
-		Result_direct[I[i]] += val[i] * V[J[i]];
-	}
-	end_time = myclock::now();
-	cout << "one thread, direct calculated" << " in " << duration_cast<milliseconds>(end_time - start_time).count() << "ms" << endl;
-
-	// one thread, CSR
-	start_time = myclock::now();
-	double* Result_CSR = new double[N];
-	memset(Result_CSR, 0, sizeof(double) * N);
-	for (int i = 0; i < N; i++) {
-		int a = row_id[i];
-		int b = row_id[i + 1];
-		while (a != b) {
-			Result_CSR[i] += value[a] * V[col[a]];
-			++a;
-		}
-	}
-	end_time = myclock::now();
-	cout << "one thread, CSR calculated" << " in " << duration_cast<milliseconds>(end_time - start_time).count() << "ms" << endl;
-
-	// compare direct and CSR
-	double Max_diff = 0;
-	for (int i = 0; i < N; i++)
-		Max_diff = max(Max_diff, abs(Result_direct[i] - Result_CSR[i]));
-	cout << "Max_diff in result vectors (direct, CSR): " << Max_diff << endl;
-
-	N = 0; M = 0; nz = 0;
-	delete[] V;
-
-	delete[] Result_direct;
-	delete[] Result_CSR;
-	*/
 	return 0;
 }
