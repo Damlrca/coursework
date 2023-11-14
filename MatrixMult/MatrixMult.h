0dff8f2db1f2b85e701b9d82f4862d1d197091f2
Damlrca <sir.damirca@yandex.ru>
2023-11-15 00:14:57 +0300

    cmake structure for mastrixmult and matrixmulttest

---
+++ MatrixMult/MatrixMult.h
@@ -0,0 +1,4 @@
+#ifndef MATRIX_MULT_H
+#define MATRIX_MULT_H
+
+#endif // !MATRIX_MULT_H
---
+++ MatrixMultTest/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello matrixmulttest" << endl;
+	return 0;
+}
