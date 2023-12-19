40a39012a71bb2358d7e359147ccde59f4e54244
Damlrca <sir.damirca@yandex.ru>
2023-12-19 03:40:16 +0300

    basic_test

--- MatrixMultTests/main.cpp
+++ MatrixMultTests/main.cpp
@@ -35,92 +35,106 @@ void comp_algo(const char* algo_name_1, const char* algo_name_2, matrix_CSR& C_c
 	}
 }
 
+void basic_test(matrix_CSR& matrA_CSR, matrix_CSR& matrB_CSR) {
+	// matrix_mult_mkl
+
+	//cout << "C = A * B" << endl;
+	matrix_CSR matrC_CSR;
+	test_algo("matrix_mult_mkl", matrix_mult_mkl, matrA_CSR, matrB_CSR, matrC_CSR);
+	//cout << "Matrix C: N: " << matrC_CSR.N << ", M: " << matrC_CSR.M << ", nz: " << matrC_CSR.row_id[matrC_CSR.N] << endl;
+
+	transpose_this_CSR(&matrC_CSR);
+	transpose_this_CSR(&matrC_CSR);
+	cout << endl;
+
+	transpose_this_CSR(&matrB_CSR); // !!! transpose matrix B !!!
+
+	// matrC_CSR_naive
+	for (int i = 0; i < 3; i++) {
+		matrix_CSR matrC_CSR_naive;
+		//cout << "C_naive = A * B" << endl;
+		test_algo("matrix_mult_naive", matrix_mult_naive, matrA_CSR, matrB_CSR, matrC_CSR_naive);
+		//cout << "Matrix C_naive: N: " << matrC_CSR_naive.N << ", M: " << matrC_CSR_naive.M << ", nz: " << matrC_CSR_naive.row_id[matrC_CSR_naive.N] << endl;
+
+		transpose_this_CSR(&matrC_CSR_naive);
+		transpose_this_CSR(&matrC_CSR_naive);
+		comp_algo("matrC_CSR", "matrC_CSR_naive", matrC_CSR, matrC_CSR_naive);
+		delete_CSR(&matrC_CSR_naive);
+	}
+	cout << endl;
+
+	// matrC_CSR_naive_2
+	transpose_this_CSR(&matrA_CSR); // !!! matrA_CSR should be sorted
+	transpose_this_CSR(&matrA_CSR);
+	// !!! (matrB_CSR is already transposed and sorted)
+	for (int i = 0; i < 3; i++) {
+		matrix_CSR matrC_CSR_naive_2;
+		//cout << "C_naive_2 = A * B" << endl;
+		test_algo("matrix_mult_naive_2", matrix_mult_naive_2, matrA_CSR, matrB_CSR, matrC_CSR_naive_2);
+		//cout << "Matrix C_naive_2: N: " << matrC_CSR_naive_2.N << ", M: " << matrC_CSR_naive_2.M << ", nz: " << matrC_CSR_naive_2.row_id[matrC_CSR_naive_2.N] << endl;
+
+		transpose_this_CSR(&matrC_CSR_naive_2);
+		transpose_this_CSR(&matrC_CSR_naive_2);
+		comp_algo("matrC_CSR", "matrC_CSR_naive_2", matrC_CSR, matrC_CSR_naive_2);
+		delete_CSR(&matrC_CSR_naive_2);
+	}
+	cout << endl;
+
+	// matrC_CSR_naive_3
+	for (int i = 0; i < 3; i++) {
+		matrix_CSR matrC_CSR_naive_3;
+		//cout << "C_naive_3 = A * B" << endl;
+		test_algo("matrix_mult_naive_3", matrix_mult_naive_3, matrA_CSR, matrB_CSR, matrC_CSR_naive_3);
+		//cout << "Matrix C_naive_3: N: " << matrC_CSR_naive_3.N << ", M: " << matrC_CSR_naive_3.M << ", nz: " << matrC_CSR_naive_3.row_id[matrC_CSR_naive_3.N] << endl;
+
+		transpose_this_CSR(&matrC_CSR_naive_3);
+		transpose_this_CSR(&matrC_CSR_naive_3);
+		comp_algo("matrC_CSR", "matrC_CSR_naive_3", matrC_CSR, matrC_CSR_naive_3);
+		delete_CSR(&matrC_CSR_naive_3);
+	}
+	cout << endl;
+
+	delete_CSR(&matrC_CSR);
+}
+
 int main() {
-	//matrix_COO matrA_COO;
-	//read_matrix_BIN(fileA, &matrA_COO);
-	//matrix_COO matrB_COO;
-	//read_matrix_BIN(fileB, &matrB_COO);
+	cout << "-------------------------------------------" << endl;
+	cout << "test uniform matrix 10'000 x 10'000, k = 15" << endl;
+	cout << "-------------------------------------------" << endl;
+	cout << endl;
+
 	matrix_CSR matrA_CSR;
-	//convert_COO_to_CSR(&matrA_COO, &matrA_CSR);
-	//generate_uniform_square_sparse_matrix_CSR(20'000, 25, matrA_CSR);
 	generate_uniform_square_sparse_matrix_CSR(10'000, 15, matrA_CSR);
-	//generate_nonuniform_square_sparse_matrix_CSR(20'000, 0, 50, matrA_CSR);
-	//cout << "A (" << fileA << ")" << endl;
 	cout << "Matrix A generated successfully" << endl;
 	cout << "Matrix A: N: " << matrA_CSR.N << ", M: " << matrA_CSR.M << ", nz: " << matrA_CSR.row_id[matrA_CSR.N] << endl;
 	matrix_CSR matrB_CSR;
-	//convert_COO_to_CSR(&matrB_COO, &matrB_CSR);
-	//generate_uniform_square_sparse_matrix_CSR(20'000, 25, matrB_CSR);
 	generate_uniform_square_sparse_matrix_CSR(10'000, 15, matrB_CSR);
-	//generate_nonuniform_square_sparse_matrix_CSR(20'000, 0, 50, matrB_CSR);
-	//cout << "B (" << fileB << ")" << endl;
 	cout << "Matrix B generated successfully" << endl;
 	cout << "Matrix B: N: " << matrB_CSR.N << ", M: " << matrB_CSR.M << ", nz: " << matrB_CSR.row_id[matrB_CSR.N] << endl;
 	cout << endl;
 
-	// matrix_mult_mkl
-
-	cout << "C = A * B" << endl;
-	matrix_CSR matrC_CSR;
-	test_algo("matrix_mult_mkl", matrix_mult_mkl, matrA_CSR, matrB_CSR, matrC_CSR);
-	cout << "Matrix C: N: " << matrC_CSR.N << ", M: " << matrC_CSR.M << ", nz: " << matrC_CSR.row_id[matrC_CSR.N] << endl;
-
-	transpose_this_CSR(&matrC_CSR);
-	transpose_this_CSR(&matrC_CSR);
-	cout << endl;
-
-	//
-
-	transpose_this_CSR(&matrB_CSR); // !!! transpose matrix B !!!
-
-	// matrC_CSR_naive_3
-
-	matrix_CSR matrC_CSR_naive_3;
-	cout << "C_naive_3 = A * B" << endl;
-	test_algo("matrix_mult_naive_3", matrix_mult_naive_3, matrA_CSR, matrB_CSR, matrC_CSR_naive_3);
-	cout << "Matrix C_naive_3: N: " << matrC_CSR_naive_3.N << ", M: " << matrC_CSR_naive_3.M << ", nz: " << matrC_CSR_naive_3.row_id[matrC_CSR_naive_3.N] << endl;
-
-	transpose_this_CSR(&matrC_CSR_naive_3);
-	transpose_this_CSR(&matrC_CSR_naive_3);
-	comp_algo("matrC_CSR", "matrC_CSR_naive_3", matrC_CSR, matrC_CSR_naive_3);
-	cout << endl;
-
-	// matrC_CSR_naive
-
-	matrix_CSR matrC_CSR_naive;
-	cout << "C_naive = A * B" << endl;
-	test_algo("matrix_mult_naive", matrix_mult_naive, matrA_CSR, matrB_CSR, matrC_CSR_naive);
-	cout << "Matrix C_naive: N: " << matrC_CSR_naive.N << ", M: " << matrC_CSR_naive.M << ", nz: " << matrC_CSR_naive.row_id[matrC_CSR_naive.N] << endl;
-	
-	transpose_this_CSR(&matrC_CSR_naive);
-	transpose_this_CSR(&matrC_CSR_naive);
-	comp_algo("matrC_CSR", "matrC_CSR_naive", matrC_CSR, matrC_CSR_naive);
-	cout << endl;
-
-	transpose_this_CSR(&matrA_CSR); // !!! matrA_CSR should be sorted
-	transpose_this_CSR(&matrA_CSR);
-	// !!! (matrB_CSR is already transposed and sorted)
-
-	// matrC_CSR_naive_2
-
-	matrix_CSR matrC_CSR_naive_2;
-	cout << "C_naive_2 = A * B" << endl;
-	test_algo("matrix_mult_naive_2", matrix_mult_naive_2, matrA_CSR, matrB_CSR, matrC_CSR_naive_2);
-	cout << "Matrix C_naive_2: N: " << matrC_CSR_naive_2.N << ", M: " << matrC_CSR_naive_2.M << ", nz: " << matrC_CSR_naive_2.row_id[matrC_CSR_naive_2.N] << endl;
-
-	transpose_this_CSR(&matrC_CSR_naive_2);
-	transpose_this_CSR(&matrC_CSR_naive_2);
-	comp_algo("matrC_CSR", "matrC_CSR_naive_2", matrC_CSR, matrC_CSR_naive_2);
-	cout << endl;
-
-	//
+	basic_test(matrA_CSR, matrB_CSR);
 
 	delete_CSR(&matrA_CSR);
 	delete_CSR(&matrB_CSR);
-	delete_CSR(&matrC_CSR);
-	delete_CSR(&matrC_CSR_naive);
-	delete_CSR(&matrC_CSR_naive_2);
-	delete_CSR(&matrC_CSR_naive_3);
+
+	cout << "-------------------------------------------" << endl;
+	cout << "test nonuniform matrix 10'000 x 10'000, k1 = 1, k2 = 30" << endl;
+	cout << "-------------------------------------------" << endl;
+	cout << endl;
+
+	generate_nonuniform_square_sparse_matrix_CSR(10'000, 1, 30, matrA_CSR);
+	cout << "Matrix A generated successfully" << endl;
+	cout << "Matrix A: N: " << matrA_CSR.N << ", M: " << matrA_CSR.M << ", nz: " << matrA_CSR.row_id[matrA_CSR.N] << endl;
+	generate_nonuniform_square_sparse_matrix_CSR(10'000, 1, 30, matrB_CSR);
+	cout << "Matrix B generated successfully" << endl;
+	cout << "Matrix B: N: " << matrB_CSR.N << ", M: " << matrB_CSR.M << ", nz: " << matrB_CSR.row_id[matrB_CSR.N] << endl;
+	cout << endl;
+
+	basic_test(matrA_CSR, matrB_CSR);
+
+	delete_CSR(&matrA_CSR);
+	delete_CSR(&matrB_CSR);
 
 	return 0;
 }
